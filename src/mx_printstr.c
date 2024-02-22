c6a5e907522aa9e933f70c696188c80a68791b6a
kalashnikov_KN-923v <kotikfop@gmail.com>
2024-02-22 11:26:17 +0200

    race2

---
+++ src/mx_printstr.c
@@ -0,0 +1,8 @@
+#include "../inc/header.h"
+
+void mx_printstr(const char *s) {
+    write(1, s, mx_strlen(s));
+}
+
+
+
---
+++ src/mx_strdel.c
@@ -0,0 +1,10 @@
+#include "../inc/header.h"
+
+void mx_strdel(char **str) {
+    if (*str) {
+        free(*str);
+        *str = NULL;
+    }
+}
+
+
